0815a8c2b3b2d7278dfc2760950b7e8611a997d6
Trung Phan <trung.phan.code@gmail.com>
2017-06-21 09:04:05 -0700

    Change style to 4 spaces

--- solutions/lesson1/src/ChatBotProgram.java
+++ solutions/lesson1/src/ChatBotProgram.java
@@ -5,31 +5,31 @@ import java.util.Scanner;
  */
 public class ChatBotProgram {
 
-  String[] inputs = {"hi", "how are you", "what's your name"};
-  String[] responses = {"hi", "very well", "I'm Super Bot"};
-  String defaultResponse = "Sorry I don't understand";
+    String[] inputs = {"hi", "how are you", "what's your name"};
+    String[] responses = {"hi", "very well", "I'm Super Bot"};
+    String defaultResponse = "Sorry I don't understand";
 
-  public String sendMessage(String input) {
-    for (int i = 0; i < inputs.length; i = i + 1) {
-      if (inputs[i].equals(input)) {
-        return responses[i];
-      }
+    public String sendMessage(String input) {
+        for (int i = 0; i < inputs.length; i = i + 1) {
+            if (inputs[i].equals(input)) {
+                return responses[i];
+            }
+        }
+        return defaultResponse;
     }
-    return defaultResponse;
-  }
 
-  public static void main(String ... args) {
+    public static void main(String... args) {
 
-    System.out.println("Super Bot > Hi there");
+        System.out.println("Super Bot > Hi there");
 
-    ChatBotProgram program = new ChatBotProgram();
-    Scanner scanner = new Scanner(System.in);
+        ChatBotProgram program = new ChatBotProgram();
+        Scanner scanner = new Scanner(System.in);
 
-    while (true) {
-      System.out.print("Me > ");
-      String input = scanner.nextLine();
-      String response = program.sendMessage(input);
-      System.out.println("Super Bot > " + response);
+        while (true) {
+            System.out.print("Me > ");
+            String input = scanner.nextLine();
+            String response = program.sendMessage(input);
+            System.out.println("Super Bot > " + response);
+        }
     }
-  }
 }
