3d478a363a3db393779f377d2c7f218b29f78b9f
Trung Phan <trung.phan.code@gmail.com>
2017-06-21 09:09:42 -0700

    Make first lesson simpler

--- solutions/lesson1/src/ChatBotProgram.java
+++ solutions/lesson1/src/ChatBotProgram.java
@@ -5,30 +5,24 @@ import java.util.Scanner;
  */
 public class ChatBotProgram {
 
-    String[] inputs = {"hi", "how are you", "what's your name"};
-    String[] responses = {"hi", "very well", "I'm Super Bot"};
-    String defaultResponse = "Sorry I don't understand";
-
-    public String sendMessage(String input) {
-        for (int i = 0; i < inputs.length; i = i + 1) {
-            if (inputs[i].equals(input)) {
-                return responses[i];
-            }
-        }
-        return defaultResponse;
-    }
-
     public static void main(String... args) {
-
         System.out.println("Super Bot > Hi there");
 
-        ChatBotProgram program = new ChatBotProgram();
-        Scanner scanner = new Scanner(System.in);
+        String[] expectedInputs = {"hi", "how are you", "what's your name"};
+        String[] responses = {"hi", "very well", "I'm Super Bot"};
+        String defaultResponse = "Sorry I don't understand";
 
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("Me > ");
             String input = scanner.nextLine();
-            String response = program.sendMessage(input);
+            String response = defaultResponse;
+            for (int i = 0; i < expectedInputs.length; i = i + 1) {
+                if (expectedInputs[i].equals(input)) {
+                    response = responses[i];
+                    break;
+                }
+            }
             System.out.println("Super Bot > " + response);
         }
     }
